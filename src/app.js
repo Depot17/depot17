917b5c4b46007acd6764003c11c12c47427dea45
Sophia Duan <duansophia8@gmail.com>
2025-08-18 13:26:14 -0400

    connect form to google sheets

--- src/app.js
+++ src/app.js
@@ -8,3 +8,22 @@ gsap.from(".box", {
     x: 800, 
     duration: 1,
 });
+
+const scriptURL ="https://script.google.com/macros/s/AKfycbwg3qWCELhFNx4MeGwGhXARYLqD7PRK8KqGVKQTYi-ZoL40rzcNIZw0-wqqDNooSXfIdw/exec";
+
+const form = document.forms["email-form"];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var formData = new FormData(form);
+
+    fetch(scriptURL, {method: "POST", body: formData})
+    .then((response) => {
+        console.log("yaay");
+        form.reset();
+    })
+
+    .catch((error) => {
+        console.log("boooo");
+    });
+})
