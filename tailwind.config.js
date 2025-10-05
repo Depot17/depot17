463ee673515f68f571252ee1a629c45c78b3155a
Sophia Duan <duansophia8@gmail.com>
2025-10-04 23:18:38 -0400

    add flash animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      keyframes: {
+        blink: {
+          '0%, 100%': {opacity:'0'},
+          '50%': {opacity:'1'},
+        },
+      },
+      animation: {
+        blink: 'blink 1s infinite',
+      },
         screens: {
             '3xl':'1600px',
         },
