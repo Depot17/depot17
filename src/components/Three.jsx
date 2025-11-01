94852652895582c369bc9665a754a49289081ef0
Sophia Duan <duansophia8@gmail.com>
2025-11-01 13:21:54 -0400

    possibly make gltf work

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -1,83 +1,46 @@
-
 import * as THREE from "three";
-import React, { useEffect, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
-import { OBJLoader } from "three-stdlib";
-import { MTLLoader } from "three-stdlib";
-// import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-// import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
-// import { EffectComposer, Outline } from '@react-three/postprocessing';
-// import { DDSLoader } from "three-stdlib";
-// // import { Suspense } from "react";
-// import { Shader } from "./Shader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import React, { useEffect, Suspense } from "react";
 
-    //  const size = window.innerWidth 
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "/3d/claw.gltf");
+//   return <primitive object={gltf.scene} scale={10} />
 
-    const Scene = () => {
-        const materials = useLoader(MTLLoader, "/3d/Claw.mtl")
-        const obj = useLoader(OBJLoader, '/3d/Claw.obj', (loader) => {
-            materials.preload();
-            loader.setMaterials(materials);
+  useEffect(() => {
+    const box = new THREE.Box3().setFromObject(gltf.scene);
+    const center = box.getCenter(new THREE.Vector3());
+    gltf.scene.position.sub(center);
+  }, [gltf]);
 
-
-        // useRef(() => {
-        //     obj.traverse((child) => {
-        //     if (child.isMesh) outlineRefs.current.push(child);
-        //     });
-        // });
-});
-        useEffect(() => {
-            const box = new THREE.Box3().setFromObject(obj);
-            const center = new THREE.Vector3();
-            box.getCenter(center);
-
-            obj.position.x -= center.x;
-            obj.position.y -= center.y;
-            obj.position.z -= center.z;
-            }, [obj]);
-        
-        // this is temporary, it doesn't currently change if the window size is being adjusted
-            if(window.innerWidth < 1024){
-        return <primitive object={obj} scale={8} rotation={[-0.8, 0.5,0.2]} />
-
-            }
+    if(window.innerWidth < 1024){
+        console.log('okay small');
+        return <primitive object={gltf.scene} scale={8} rotation={[-0.8, 0.5,0.2]} />
+        }
+            
             else {
-        return <primitive object={obj} scale={12} rotation={[-0.8, 0.5,0.2]} />
+                        console.log('okay big');
+
+        return <primitive object={gltf.scene} scale={12} rotation={[-0.8, 0.5,0.2]} />
 
             }
 
-    };     
 
-    export default function App(){
-        return (
-            <Canvas orthographic camera={{zoom:100, position: [0,0,10],}} style={{ width: "1000px", height: "700px"}}>
-                <ambientLight intensity={0.8} />
-                <directionalLight color="yellow" position={[0, 0, 6]} intensity={1.2} />
-                {/* <directionalLight color="white" position={[-5, -5, -2]} intensity={0.5} /> */}
-                <Suspense fallback={null}>
-                <Scene />
+};
 
-                </Suspense>
-  
-                <OrbitControls enableZoom={false}></OrbitControls>
-            </Canvas>
-    );
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas orthographic camera={{zoom:100, position: [0,0,10],}} style={{ width: "1000px", height: "700px"}}>
+        <ambientLight intensity={0.8} />
+        <directionalLight color="yellow" position={[0, 0, 10]} intensity={1.2} />
+        <Suspense fallback={null}>
+          <Model />
+            <OrbitControls enableZoom={false}></OrbitControls>        
+            </Suspense>
+      </Canvas>
+    </div>
+  );
 }
-
-//     export default function App(){
-//         return (
-//             <Canvas orthographic camera={{zoom:100, position: [0,0,10],}} style={{ width: "1000px", height: "700px"}}>
-        
-//                 <ambientLight intensity={0.7} />
-//                 <directionalLight color="white" position={[0, 0, 6]} intensity={2} />
-//                 <Suspense fallback={null}>
-//                 <Scene />
-
-//                 </Suspense>
-  
-//                 <OrbitControls enableZoom={false}></OrbitControls>
-//             </Canvas>
-//     );
-// }
