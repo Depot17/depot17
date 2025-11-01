72abdc50cf5092b0b316c03f68b29050be1766b9
Sophia Duan <duansophia8@gmail.com>
2025-11-01 14:13:52 -0400

    on mobile model is behind elements

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -16,16 +16,12 @@ const Model = () => {
   }, [gltf]);
 
     if(window.innerWidth < 1024){
-        console.log('okay small');
-        return <primitive object={gltf.scene} scale={8} rotation={[-0.8, 0.5,0.2]} />
+        return <primitive object={gltf.scene} scale={9} rotation={[-0.8, 0.5,0.2]} />
         }
             
-            else {
-                        console.log('okay big');
-
+    else {
         return <primitive object={gltf.scene} scale={12} rotation={[-0.8, 0.5,0.2]} />
-
-            }
+    }
 
 
 };
@@ -34,8 +30,8 @@ export default function App() {
   return (
     <div className="App">
       <Canvas orthographic camera={{zoom:100, position: [0,0,10],}} style={{ width: "1000px", height: "700px"}}>
-        <ambientLight intensity={0.8} />
-        <directionalLight color="yellow" position={[0, 0, 10]} intensity={1.2} />
+        <ambientLight intensity={1} />
+        <directionalLight color="white" position={[0, 0, 10]} intensity={0.8} />
         <Suspense fallback={null}>
           <Model />
             <OrbitControls enableZoom={false}></OrbitControls>        
